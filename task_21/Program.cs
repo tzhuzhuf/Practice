fdd59b9d8255c47e4ce6ba7063253ae0dbe392a6
Student406-11 <Student406-11@NPK-102O-2>
2024-11-21 13:40:06 +0700

    completed task 21

---
+++ task_21/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первую дату в формате year.month.day: ");
+string input1 = Console.ReadLine().Trim();
+int[] array1 = input1.Split('.').Select(x => int.Parse(x)).ToArray();
+DateTime date1 = new DateTime(array1[0], array1[1], array1[2]);
+
+Console.Write("Введите вторую дату в том же формате: ");
+string input2 = Console.ReadLine().Trim();
+int[] array2 = input2.Split('.').Select(x => int.Parse(x)).ToArray();
+DateTime date2 = new DateTime(array2[0], array2[1], array2[2]);
+
+Console.WriteLine(date1.Subtract(date2));
+
+
---
+++ task_22/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Выбери какие данные проверять (");
+
+    }
+}
